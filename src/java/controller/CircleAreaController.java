c5d583d411be6246ef3068020cc42517bd9d9a9f
User <User@10.30.8.243>
2014-02-03 11:20:14 -0600

    results pages are set up and functional for all 3 calculations

--- src/java/controller/CircleAreaController.java
+++ src/java/controller/CircleAreaController.java
@@ -8,11 +8,13 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.CircleAreaCalcualtion;
 
 /**
  *
@@ -21,31 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "CircleAreaController", urlPatterns = {"/CircleAreaController"})
 public class CircleAreaController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CircleAreaController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CircleAreaController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+ private static final String RESULT_PAGE_TWO = "resultsTwo.jsp";
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -59,7 +37,7 @@ public class CircleAreaController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
     }
 
     /**
@@ -73,7 +51,23 @@ public class CircleAreaController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+        response.setContentType("text/html");
+
+            String r = request.getParameter("radius");
+            
+            double radius = Double.parseDouble(r);
+            
+            CircleAreaCalcualtion ca = new CircleAreaCalcualtion();
+            double resultsTwo = ca.getArea(radius);
+
+            request.setAttribute("resultsTwo", resultsTwo);
+            
+            RequestDispatcher view =
+                request.getRequestDispatcher(RESULT_PAGE_TWO);
+            view.forward(request, response);
+            
+            
     }
 
     /**
